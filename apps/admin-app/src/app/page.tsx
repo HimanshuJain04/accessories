829b428c00652ce1ee47a7332127c9a49a5555d9
Himanshu <himanshujain5690174@gmail.com>
2024-07-25 00:04:51 +0530

    chore: added user, admin, api apps

---
+++ apps/admin-app/src/app/page.tsx
@@ -0,0 +1,11 @@
+import Image from "next/image";
+import { Input } from "ui";
+
+export default function Home() {
+  return (
+   <div>
+      Admin-main-app
+      <Input/>
+   </div>
+  );
+}
---
+++ apps/user-app/src/app/page.tsx
@@ -0,0 +1,11 @@
+import Image from "next/image";
+import {Input} from "ui"
+
+export default function Home() {
+  return (
+    <div>
+      User-main-app
+      <Input/>
+    </div>
+  );
+}
